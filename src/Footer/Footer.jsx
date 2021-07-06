b732661a8954c043d9f969d320784f1c2e341cb4
Utsav2931 <18ce089@charusat.edu.in>
2021-07-06 23:22:39 +0530

    footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,47 +3,109 @@ import './styles.css';
 //import useStyles from './styles.css';
 
 const Footer = () => {
-    return(
+    return (
+        //     <>
+        //     <footer className="footer-area">
+        //     <div className="footer-wave-box">
+        //         <div className="footer-wave footer-animation"></div>
+        //     </div>
+        //     <div className="main">
+        //         <div className="footer">
+        //             <div className="single-footer">
+        //                 <h4>Register Here</h4>
+        //                 <p>Enter your Email to get our news updates</p>
+        //                 <form action="" className="email-form">
+        //                     <input type="text" className="txtb" placeholder="Enter Your Email"/>
+        //                     <input type="submit" className="btn" value="submit"/>
+        //                 </form>
+        //             </div>
+        //             <div className="single-footer">
+        //                 <h4>contact us</h4>
+        //                 <ul>
+        //                     <li><a href=""><i className="fas fa-map-marker-alt"></i> North Street, Sydney, Australia</a></li>
+        //                     <li><a href=""><i className="fas fa-mobile-alt"></i> +88 0123 456 789</a></li>
+        //                     <li><a href=""><i className="far fa-envelope"></i> info@demo.com</a></li>
+        //                     <li><a href=""><i className="fas fa-globe-europe"></i> www.demo.com</a></li>
+        //                 </ul>
+        //             </div>
+        //             <div className="single-footer">
+        //             <div className="map">
+        //             <a href="https://maps.app.goo.gl/siYAXWiJ9TJnfn91A">
+        //                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.0564600467847!2d73.06360621496779!3d23.02169908495345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e6d757926c471%3A0xe3232aeabc8239c8!2sRamesh%20Brothers!5e0!3m2!1sen!2sin!4v1625589891932!5m2!1sen!2sin"  style={{width:"600", height:"450", border:0, allowfullscreen:"", loading:"lazy"}}></iframe>
+        //                 {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29377.489843752923!2d73.05765097310908!3d23.016933431604734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e6d58936de175%3A0xb72304f8191d7c71!2sKapadvanj%2C%20Gujarat%20387620!5e0!3m2!1sen!2sin!4v1624594716063!5m2!1sen!2sin" style={{width:"350", height:"250", border:0, allowfullscreen:"", loading:"lazy"}}></iframe> */}
+        //             </a>
+        //             </div>
+        //             </div>
+        //         </div>
+        //         <div className="copy">
+        //             <p>&copy; 2021 all rights reserved</p>
+        //         </div>
+        //     </div>
+        // </footer>
+        //     </>
         <>
-        <footer className="footer-area">
-        <div className="footer-wave-box">
-            <div className="footer-wave footer-animation"></div>
-        </div>
-        <div className="main">
             <div className="footer">
-                <div className="single-footer">
-                    <h4>Register Here</h4>
-                    <p>Enter your Email to get our news updates</p>
-                    <form action="" className="email-form">
-                        <input type="text" className="txtb" placeholder="Enter Your Email"/>
-                        <input type="submit" className="btn" value="submit"/>
-                    </form>
-                </div>
-                <div className="single-footer">
-                    <h4>contact us</h4>
-                    <ul>
-                        <li><a href=""><i className="fas fa-map-marker-alt"></i> North Street, Sydney, Australia</a></li>
-                        <li><a href=""><i className="fas fa-mobile-alt"></i> +88 0123 456 789</a></li>
-                        <li><a href=""><i className="far fa-envelope"></i> info@demo.com</a></li>
-                        <li><a href=""><i className="fas fa-globe-europe"></i> www.demo.com</a></li>
-                    </ul>
-                </div>
-                <div className="single-footer">
-                <div className="map">
-                <a href="https://maps.app.goo.gl/siYAXWiJ9TJnfn91A">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.0564600467847!2d73.06360621496779!3d23.02169908495345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e6d757926c471%3A0xe3232aeabc8239c8!2sRamesh%20Brothers!5e0!3m2!1sen!2sin!4v1625589891932!5m2!1sen!2sin"  style={{width:"600", height:"450", border:0, allowfullscreen:"", loading:"lazy"}}></iframe>
-                    {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29377.489843752923!2d73.05765097310908!3d23.016933431604734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e6d58936de175%3A0xb72304f8191d7c71!2sKapadvanj%2C%20Gujarat%20387620!5e0!3m2!1sen!2sin!4v1624594716063!5m2!1sen!2sin" style={{width:"350", height:"250", border:0, allowfullscreen:"", loading:"lazy"}}></iframe> */}
-                </a>
-                </div>
+                <div className="contain">
+                    <div className="col">
+                        <h1>Company</h1>
+                        <ul>
+                            <li>About</li>
+                            <li>Mission</li>
+                            <li>Services</li>
+                            <li>Social</li>
+                            <li>Get in touch</li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h1 className = "h11">Products</h1>
+                        <ul className = "u11">
+                            <li>About</li>
+                            <li>Mission</li>
+                            <li>Services</li>
+                            <li>Social</li>
+                            <li>Get in touch</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h1>Accounts</h1>
+                        <ul>
+                            <li>About</li>
+                            <li>Mission</li>
+                            <li>Services</li>
+                            <li>Social</li>
+                            <li>Get in touch</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h1 className="h22">Resources</h1>
+                        <ul className="u22">
+                            <li>Webmail</li>
+                            <li>Redeem code</li>
+                            <li>WHOIS lookup</li>
+                            <li>Site map</li>
+                            <li>Web templates</li>
+                            <li>Email templates</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h1>Support</h1>
+                        <ul>
+                            <a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJNsvKcrnNbmDtxXpChwxrPqMsrMhpMwmtdLWpdHNNnqbclMbfXlGpLjMgDDSvpcqZxptVB"><li>Contact us</li></a>
+                            <a href="https://wa.me/919099672704"><li>Web chat</li></a>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h1>Support</h1>
+                        <a href="https://maps.app.goo.gl/siYAXWiJ9TJnfn91A">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.0564600467847!2d73.06360621496779!3d23.02169908495345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e6d757926c471%3A0xe3232aeabc8239c8!2sRamesh%20Brothers!5e0!3m2!1sen!2sin!4v1625589891932!5m2!1sen!2sin" style={{ width: "600", height: "450", border: 0, allowfullscreen: "", loading: "lazy" }}></iframe>
+                            {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29377.489843752923!2d73.05765097310908!3d23.016933431604734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e6d58936de175%3A0xb72304f8191d7c71!2sKapadvanj%2C%20Gujarat%20387620!5e0!3m2!1sen!2sin!4v1624594716063!5m2!1sen!2sin" style={{width:"350", height:"250", border:0, allowfullscreen:"", loading:"lazy"}}></iframe> */}
+                        </a>
+                    </div>
                 </div>
             </div>
-            <div className="copy">
-                <p>&copy; 2021 all rights reserved</p>
-            </div>
-        </div>
-    </footer>
+
         </>
-    ) 
+    )
 };
 
 export default Footer;
