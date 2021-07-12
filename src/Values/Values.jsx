4db7d97082ae6a0c7ebfd613101e9c22aecf3264
Utsav2931 <18ce089@charusat.edu.in>
2021-07-12 18:38:29 +0530

    responsive

--- src/Values/Values.jsx
+++ src/Values/Values.jsx
@@ -27,74 +27,74 @@ const Values = () => {
             </div>
             </div> */}
             <div className="info-circle">
-    <h1 style={{font: "32pt Arial", textAlign: "center", fontWeight: "normal", marginBottom: "48px"}}>Our Values</h1>
+                <h1 style={{ font: "32pt Arial", textAlign: "center", fontWeight: "normal", marginBottom: "48px" }}>Our Values</h1>
 
-    <div className="outer-circle">
-        <div className="inner-circle">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxnbLXyQeqUP_kFrLre4orNjTkqhnIJYLj1Q&usqp=CAU" className="image" />
-        </div>
-    </div>
+                <div className="outer-circle">
+                    <div className="inner-circle">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxnbLXyQeqUP_kFrLre4orNjTkqhnIJYLj1Q&usqp=CAU" className="image" />
+                    </div>
+                </div>
 
-    <div className="info-blocks">
-        <div className="info-block clearfix">
-            <div className="info-icon">
-                <EqualizerIcon/>
-            </div>
-            <div className="info-text">
-                <h5 className="title text-upper">Core Values</h5>
-                <p className="text">The name 'Ramesh Brothers' symbolizes the co-existence of one and all creating long lasting relationships in society by dissolving differnces over a good cup of tea.</p>
-            </div>
-        </div>
+                <div className="info-blocks">
+                    <div className="info-block clearfix">
+                        <div className="info-icon">
+                            <EqualizerIcon />
+                        </div>
+                        <div className="info-text">
+                            <h5 className="title text-upper">Core Values</h5>
+                            <p className="text">The name 'Ramesh Brothers' symbolizes the co-existence of one and all creating long lasting relationships in society by dissolving differnces over a good cup of tea.</p>
+                        </div>
+                    </div>
 
-        <div className="info-block clearfix">
-            <div className="info-icon">
-                <AdjustIcon style={{marginTop: "-50px"}}/>
-            </div>
-            <div className="info-text">
-                <h5 className="title text-upper">Objective</h5>
-                <p className="text">We serve the best quality of products like tea, namkeen, spices, etc. consistently throughout the year to one and all.</p>
-            </div>
-        </div>
+                    <div className="info-block clearfix">
+                        <div className="info-icon">
+                            <AdjustIcon style={{ marginTop: "-50px" }} />
+                        </div>
+                        <div className="info-text">
+                            <h5 className="title text-upper">Objective</h5>
+                            <p className="text">We serve the best quality of products like tea, namkeen, spices, etc. consistently throughout the year to one and all.</p>
+                        </div>
+                    </div>
 
-        <div className="info-block clearfix">
-            <div className="info-icon">
-            <VisibilityIcon/>
-            </div>
-            <div className="info-text">
-                <h5 className="title">Vision</h5>
-                <p className="text">Serving India based reputed brands and delivering superior value to all its stakeholders.</p>
-            </div>
-        </div>
+                    <div className="info-block clearfix">
+                        <div className="info-icon">
+                            <VisibilityIcon />
+                        </div>
+                        <div className="info-text">
+                            <h5 className="title">Vision</h5>
+                            <p className="text">Serving India based reputed brands and delivering superior value to all its stakeholders.</p>
+                        </div>
+                    </div>
 
-        <div className="info-block clearfix">
-            <div className="info-icon">
-                <FlagIcon/> 
-            </div>
-            <div className="info-text">
-                <h5 className="title">Mission</h5>
-                <p className="text">Dealing with Indian companies and adding sweetness to relationships by serving the best quality products in every corner we can through retail and quick service.</p>
-            </div>
-        </div>
+                    <div className="info-block clearfix">
+                        <div className="info-icon">
+                            <FlagIcon />
+                        </div>
+                        <div className="info-text">
+                            <h5 className="title">Mission</h5>
+                            <p className="text">Dealing with Indian companies and adding sweetness to relationships by serving the best quality products in every corner we can through retail and quick service.</p>
+                        </div>
+                    </div>
 
-        <div className="info-block clearfix">
-            <div className="info-icon">
-                <FavoriteIcon/>
-            </div>
-            <div className="info-text title-only">
-                <h5 className="title text-upper">In other words, Ramesh Brothers firm stands for relationships and harmony.</h5>
-            </div>
-        </div>
+                    <div className="info-block clearfix">
+                        <div className="info-icon">
+                            <FavoriteIcon />
+                        </div>
+                        <div className="info-text title-only">
+                            <h5 className="title text-upper">In other words, Ramesh Brothers firm stands for relationships and harmony.</h5>
+                        </div>
+                    </div>
 
-        <div className="info-block clearfix">
-            <div className="info-icon">
-                <LoyaltyIcon/>
+                    <div className="info-block clearfix">
+                        <div className="info-icon">
+                            <LoyaltyIcon />
+                        </div>
+                        <div className="info-text title-only">
+                            <h5 className="title">Respect for all.</h5>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="info-text title-only">
-                <h5 className="title">Respect for all.</h5>
-            </div>
-        </div>
-    </div>
-</div>
 
         </>
     )
